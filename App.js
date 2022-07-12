c2557a951327999d9f34fd15029a9204a594b0db
Dvir kopolovich <64788488+dvirko@users.noreply.github.com>
2022-07-13 01:10:00 +0300

    setup

--- App.js
+++ App.js
@@ -1,40 +1,38 @@
 const express = require('express');
 var bodyParser = require('body-parser');
-const path = require('path');
 var mongo = require('mongodb');
 const cors = require('cors');
 const app = express();
 app.use(cors());
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = mongo.MongoClient;
 var url = "mongodb+srv://dvir:dvirko1221@data.my748.mongodb.net/?retryWrites=true&w=majority";
 
+app.get("/image", urlencodedParser, (req, res) => {
+    var requestSettings = {
+        url: 'https://www.google.com/images/srpr/logo11w.png',
+        method: 'GET',
+        encoding: null
+    };
+
+    req(requestSettings, function(error, response, body) {
+        res.set('Content-Type', 'image/png');
+        res.send(body);
+    });
+});
 
 app.get('/', urlencodedParser, (req, res) => {
 MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         var dbo = db.db("TOMATACTI_DB");
-        dbo.collection("person").find({}).toArray(function(err, result) {
+        dbo.collection("person").find({}, { projection: { _id: 0} }).toArray(function(err, result) {
             if (err) throw err;
             res.json(result);
             db.close();
           });
         });
 });
-
-app.get('/name/:tagId', urlencodedParser, (req, res) => {
-const id = req.params.tagId;
-MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("TOMATACTI_DB");
-        dbo.collection("person").find({id}).toArray(function(err, result) {
-            if (err) throw err;
-            res.json(result);
-            db.close();
-          });
-        });
-});
-    
+   
 app.post('/', urlencodedParser, (req, res) => {
     console.log('Got body:', req.body);
     res.sendStatus(200);
